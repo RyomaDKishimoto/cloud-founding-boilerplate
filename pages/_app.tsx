57bc14dd3b6cc8c4798301c3b01fe0b9b8deb834
RyomaDKishimoto <ryoma20151225@gmail.com>
2022-02-15 00:16:47 +0900

    darkモード用修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import siteMetadata from "@/data/siteMetadata";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
