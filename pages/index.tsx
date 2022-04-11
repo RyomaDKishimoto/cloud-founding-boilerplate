d0e51fd71bf1d23561b7c999799197a7bba7078c
ryomaD1225 <ryoma20151225@gmail.com>
2022-04-12 01:52:02 +0900

    prisma追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import { Donate } from "@prisma/client";
 import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import prisma from "../lib/prisma";
 import CtaSection from "@/components/CtaSection";
 import MiddleContainer from "@/components/MiddleContainer";
 import TopContainer from "@/components/TopContainer";
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const amount = await prisma.donate.findFirst({ where: { id: 1 } });
+  const jsonDonate = JSON.parse(JSON.stringify(amount));
+
+  return {
+    props: jsonDonate,
+  };
+};
 const Home: NextPage = () => {
   return (
     <div>
